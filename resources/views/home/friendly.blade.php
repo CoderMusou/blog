0d89d5391838c1890b2dad005e61fa4aa0cfd744
wseek <610075792@qq.com>
2016-08-14 19:01:01 +0800

    修改了友链

--- resources/views/home/friendly.blade.php
+++ resources/views/home/friendly.blade.php
@@ -10,28 +10,28 @@
             </div>
           @endforeach
       </div>
-    {{-- <div class="content-block comment">
-      <h2 class="title"><strong>提交友链</strong></h2>
-      <form action="message.php" method="post" class="form-inline" id="comment-form">
-        <div class="comment-title">
-          <div class="form-group">
-            <label for="messageName">网站名称：</label>
-            <input type="text" name="messageName" class="form-control" id="messageName" placeholder="异清轩技术博客">
-          </div>
-          <div class="form-group">
-            <label for="messageEmail">网站地址：</label>
-            <input type="email" name="messageEmail" class="form-control" id="messageEmail" placeholder="admin@ylsat.com">
-          </div>
-        </div>
-        <div class="comment-form">
-          <textarea placeholder="请填写网站服务内容" name="messageContent"></textarea>
-          <div class="comment-form-footer">
-            <div class="comment-form-text">请先 <a href="javascript:;">登录</a> 或 <a href="javascript:;">注册</a>，也可匿名提交友链 </div>
-            <div class="comment-form-btn">
-              <button type="submit" class="btn btn-default btn-comment">提交友链</button>
-            </div>
-          </div>
-        </div> --}}
-      </form>
-    </div>
+     {{--<div class="content-block comment">--}}
+      {{--<h2 class="title"><strong>提交友链</strong></h2>--}}
+      {{--<form action="message.php" method="post" class="form-inline" id="comment-form">--}}
+        {{--<div class="comment-title">--}}
+          {{--<div class="form-group">--}}
+            {{--<label for="messageName">网站名称：</label>--}}
+            {{--<input type="text" name="messageName" class="form-control" id="messageName" placeholder="异清轩技术博客">--}}
+          {{--</div>--}}
+          {{--<div class="form-group">--}}
+            {{--<label for="messageEmail">网站地址：</label>--}}
+            {{--<input type="email" name="messageEmail" class="form-control" id="messageEmail" placeholder="admin@ylsat.com">--}}
+          {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="comment-form">--}}
+          {{--<textarea placeholder="请填写网站服务内容" name="messageContent"></textarea>--}}
+          {{--<div class="comment-form-footer">--}}
+            {{--<div class="comment-form-text">请先 <a href="javascript:;">登录</a> 或 <a href="javascript:;">注册</a>，也可匿名提交友链 </div>--}}
+            {{--<div class="comment-form-btn">--}}
+              {{--<button type="submit" class="btn btn-default btn-comment">提交友链</button>--}}
+            {{--</div>--}}
+          {{--</div>--}}
+        {{--</div> --}}
+      {{--</form>--}}
+    {{--</div>--}}
 @endsection
